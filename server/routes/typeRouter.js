5650d1ccbb62a3a6fc43d78703c145dc2847d8bd
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-11-08 00:12:35 +0300

    appended adding types to the db

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,9 @@
+const typeController = require("../controllers/typeController")
 const Router = require('express')
 const router = new Router()
 
-router.post('/', )
-router.get('/')
-router.delete('/:id')
+router.post('/', typeController.create)
+router.get('/', typeController.getAll)
+router.delete('/:id', typeController.getOne)
 
 module.exports = router
